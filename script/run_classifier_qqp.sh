0a2f754ea5af593aaba4b82290295e9fe97dbc7a
shenfei1 <shenfei1@kingsoft.com>
2020-04-18 15:31:10 +0800

    for new branch v100-5

--- script/run_classifier_qqp.sh
+++ script/run_classifier_qqp.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
+export CUDA_VISIBLE_DEVICES=4,5
 CURRENT_DIR=`pwd`
-export DATA_DIR=$CURRENT_DIR/dataset/finetuning_data
+export DATA_DIR=$CURRENT_DIR/dataset
 export MODEL_DIR=$CURRENT_DIR/pretrained_models/electra_base
 
 python -u run_finetuning.py \
     --data-dir $DATA_DIR \
     --model-path $MODEL_DIR \
     --model-name electra_base \
-    --hparams '{"model_size": "base", "task_names": ["qqb"]}'
+    --hparams '{"model_size": "base", "task_names": ["qqp"]}'
